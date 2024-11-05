fb8db509a3dbb7abc1a1aa7392f036cdf5b44465
bill donner <billdonner@gmail.com>
2024-11-05 11:50:56 -0600

    Initial Commit

---
+++ AllAboard/AllAboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboardApp.swift
+//  AllAboard
+//
+//  Created by bill donner on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AllAboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllAboardTests/AllAboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboardTests.swift
+//  AllAboardTests
+//
+//  Created by bill donner on 11/5/24.
+//
+
+import Testing
+@testable import AllAboard
+
+struct AllAboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
